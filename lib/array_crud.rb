1ad23ef920bbef0383a042c75c98043bfc798cfc
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-21 20:26:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["diego", "beto", "isabel" "alberto"]
+  array = ["diego", "beto", "isabel", "alberto"]
 end
 
 def add_element_to_end_of_array(array, element)
